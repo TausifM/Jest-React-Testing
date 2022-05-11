fff54d7efcdc77067bdfa98dcfe13baa33e1c970
TausifM <nisargcontact2020@gmail.com>
2022-05-11 11:11:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing of React</h1>
+      <input type="text" placeholder="type name here" />
+      <button>test button</button>
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+      </ul>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("testing of app basics", () => {
+  render(<App/>);
+  const buttonElem = screen.getByRole("button", {name : "test button", exact: false})
+  expect(buttonElem).toBeInTheDocument()
+})
